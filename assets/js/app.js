e7498cb135f333f69825231951f3d666bb98872f
TruongVanPhon <truongvanphon020297@gmail.com>
2022-07-18 08:36:26 +0700

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ class App {
                 icon: 'success',
                 title: t,
                 showConfirmButton: false,
-                timer: 2500
+                timer: 9000
             })
         }
 
@@ -18,13 +18,13 @@ class App {
                 icon: 'error',
                 title: t,
                 showConfirmButton: false,
-                timer: 2500
+                timer: 4000
             })
         }
 
         static showConfirmDelete() {
             return Swal.fire({
-                title: 'Are you sure to deactive the selected customer ?',
+                title: 'Bạn Có Chắc Chắn Muốn Xóa ?',
                 icon: 'warning',
                 showCancelButton: true,
                 confirmButtonColor: '#3085d6',
@@ -34,11 +34,53 @@ class App {
         }
 
     }
+    static IziToast = class {
+        static showAlertSuccess(t) {
+            return iziToast.success({
+                    title: 'SUCCESS',
+                    position: 'topRight',
+                    timeout: 2000,
+                    message: t
+                });
+        }
+        static showAlertError(t){
+            return  iziToast.error({
+                        title: 'ERROR',
+                        position: 'topRight',
+                        timeout: 2000,
+                        message: t
+                    });
+        }
+    }
 }
 
 
 class Customer {
     constructor(id, fullName, email, phone, locationRegion, balance = 0, deleted = 0) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.locationRegion = locationRegion;
+        this.balance = balance;
+        this.deleted = deleted;
+    }
+}
+
+class Sender {
+    constructor(id, fullName, email, phone, locationRegion, balance , deleted) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.locationRegion = locationRegion;
+        this.balance = balance;
+        this.deleted = deleted;
+    }
+}
+
+class Recipient {
+    constructor(id, fullName, email, phone, locationRegion, balance , deleted) {
         this.id = id;
         this.fullName = fullName;
         this.email = email;
@@ -58,5 +100,48 @@ class LocationRegion {
         this.districtName = districtName;
         this.wardId = wardId;
         this.wardName = wardName;
+    }
+}
+
+class Deposit {
+    constructor(id, customerId, transactionAmount, createdAt, createdBy = "", updatedAT = "", updatedBy = "", deleted = 0) {
+        this.id = id;
+        this.customerId = customerId;
+        this.transactionAmount = transactionAmount;
+        this.createdAt = createdAt;
+        this.createdBy = createdBy;
+        this.updatedAt = updatedAT;
+        this.updatedBy = updatedBy;
+        this.deleted = deleted;
+    }
+}
+
+class Withdraw {
+    constructor(id, customerId, transactionAmount, createdAt, createdBy = "", updatedAT = "", updatedBy = "", deleted = 0) {
+        this.id = id;
+        this.customerId = customerId;
+        this.transactionAmount = transactionAmount;
+        this.createdAt = createdAt;
+        this.createdBy = createdBy;
+        this.updatedAt = updatedAT;
+        this.updatedBy = updatedBy;
+        this.deleted = deleted;
+    }
+}
+
+class Transfer{
+    constructor(id,createdAt,createdBy = "",deleted = 0,updatedAt = "",updatedBy = "",fees = 10,feesAmount,transactionAmount,transferAmount,recipientId,senderId){
+        this.id = id;
+        this.createdAt = createdAt;
+        this.createdBy = createdBy;
+        this.deleted = deleted;
+        this.updatedAt = updatedAt;
+        this.updatedBy = updatedBy;
+        this.fees = fees;
+        this.feesAmount = feesAmount;
+        this.transactionAmount = transactionAmount;
+        this.transferAmount = transferAmount;
+        this.recipientId = recipientId;
+        this.senderId = senderId;
     }
 }
